9cf7eafec9d630241818804306c55ddd3a5d3df4
Josh Nobus <joshnobus@gmail.com>
2023-05-29 00:44:41 +0900

    add tests for interface

--- tests/interface/test_bitflyer.py
+++ tests/interface/test_bitflyer.py
@@ -1,6 +1,9 @@
 import pytest
 
-from fxtrade.api import CodePair
+from requests.exceptions import HTTPError
+
+from fxtrade.code import CodePair
+from fxtrade.stock import Stock
 from fxtrade.interface.bitflyer import BitflyerAPI
 
 def test_BitflyerAPI():
@@ -8,3 +11,10 @@ def test_BitflyerAPI():
 
     assert api.make_code_pair_string('BTC', 'JPY') == 'BTC_JPY'
     assert api.make_code_pair_string(CodePair('BTC', 'JPY')) == 'BTC_JPY'
+
+    assert api.minimum_order_quantity(CodePair('BTC', 'JPY')) == Stock('BTC', '0.001')
+    assert api.maximum_order_quantity(CodePair('BTC', 'JPY')) == Stock('BTC', '1000')
+
+    # ネットワークに接続した状態で実施する
+    with pytest.raises(HTTPError):
+        api.download_wallet()
